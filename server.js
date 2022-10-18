f491f9070889a1e69c05e79cda7ba1f765452d82
Tseng Kuo Lun <54053565+02071819@users.noreply.github.com>
2022-10-19 02:31:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -41,6 +41,31 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/page2", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "page2.html"));
+
+  // 連接資料庫是否成功
+  conn.connect(function (error) {
+    if (error) {
+      console.log("Error");
+    } else {
+      console.log("MySql DataBase Connected");
+      // con.query("select * from average", function (error, rows) {
+      //   if (error) {
+      //     console.log("error");
+      //   } else {
+      //     console.log("success");
+      //     console.log(rows[0]);
+      //   }
+      // });
+    }
+  });
+});
+
+app.get("/result", (req, res) => {
+  res.render("result_page.ejs");
+});
+
 // 前端表單輸入
 app.post("/", function (req, res) {
   var answer1_1 = req.body.question1_1;
@@ -58,13 +83,78 @@ app.post("/", function (req, res) {
   var answer5_1 = req.body.question5_1;
   var answer5_2 = req.body.question5_2;
   var answer5_3 = req.body.question5_3;
+  var answer6_1 = req.body.question6_1;
+  var answer6_2 = req.body.question6_2;
+  var answer6_3 = req.body.question6_3;
+  var answer7_1 = req.body.question7_1;
+  var answer7_2 = req.body.question7_2;
+  var answer7_3 = req.body.question7_3;
+  var answer8_1 = req.body.question8_1;
+  var answer8_2 = req.body.question8_2;
+  var answer8_3 = req.body.question8_3;
+  var answer9_1 = req.body.question9_1;
+  var answer9_2 = req.body.question9_2;
+  var answer9_3 = req.body.question9_3;
+  var answer10_1 = req.body.question10_1;
+  var answer10_2 = req.body.question10_2;
+  var answer10_3 = req.body.question10_3;
 
-  var avg1 = (answer1_1 + answer1_2 + answer1_3) / 3;
-  var avg2 = (answer2_1 + answer2_2 + answer2_3) / 3;
-  var avg3 = (answer3_1 + answer3_2 + answer3_3) / 3;
-  var avg4 = (answer4_1 + answer4_2 + answer4_3) / 3;
-  var avg5 = (answer5_1 + answer5_2 + answer5_3) / 3;
+  var answer1_1 = parseInt(answer1_1, 10);
+  var answer1_2 = parseInt(answer1_2, 10);
+  var answer1_3 = parseInt(answer1_3, 10);
 
+  var answer2_1 = parseInt(answer2_1, 10);
+  var answer2_2 = parseInt(answer2_2, 10);
+  var answer2_3 = parseInt(answer2_3, 10);
+
+  var answer3_1 = parseInt(answer3_1, 10);
+  var answer3_2 = parseInt(answer3_2, 10);
+  var answer3_3 = parseInt(answer3_3, 10);
+
+  var answer4_1 = parseInt(answer4_1, 10);
+  var answer4_2 = parseInt(answer4_2, 10);
+  var answer4_3 = parseInt(answer4_3, 10);
+
+  var answer5_1 = parseInt(answer5_1, 10);
+  var answer5_2 = parseInt(answer5_2, 10);
+  var answer5_3 = parseInt(answer5_3, 10);
+
+  var answer6_1 = parseInt(answer6_1, 10);
+  var answer6_2 = parseInt(answer6_2, 10);
+  var answer6_3 = parseInt(answer6_3, 10);
+
+  var answer7_1 = parseInt(answer7_1, 10);
+  var answer7_2 = parseInt(answer7_2, 10);
+  var answer7_3 = parseInt(answer7_3, 10);
+
+  var answer8_1 = parseInt(answer8_1, 10);
+  var answer8_2 = parseInt(answer8_2, 10);
+  var answer8_3 = parseInt(answer8_3, 10);
+
+  var answer9_1 = parseInt(answer9_1, 10);
+  var answer9_2 = parseInt(answer9_2, 10);
+  var answer9_3 = parseInt(answer9_3, 10);
+
+  var answer10_1 = parseInt(answer10_1, 10);
+  var answer10_2 = parseInt(answer10_2, 10);
+  var answer10_3 = parseInt(answer10_3, 10);
+
+  var avg1 = (answer1_1 + answer1_2 + answer1_3)/3;
+  var avg2 = (answer2_1 + answer2_2 + answer2_3)/3;
+  var avg3 = (answer3_1 + answer3_2 + answer3_3)/3;
+  var avg4 = (answer4_1 + answer4_2 + answer4_3)/3;
+  var avg5 = (answer5_1 + answer5_2 + answer5_3)/3;
+  var avg6 = (answer6_1 + answer6_2 + answer6_3)/3;
+  var avg7 = (answer7_1 + answer7_2 + answer7_3)/3;
+  var avg8 = (answer8_1 + answer8_2 + answer8_3)/3;
+  var avg9 = (answer9_1 + answer9_2 + answer9_3)/3;
+  var avg10 = (answer10_1 + answer10_2 + answer10_3)/3;
+
+
+  console.log(answer1_1);
+  console.log(answer1_2);
+  console.log(answer1_3);
+  console.log(avg1);
   // query做SQL語法 insert是否成功
   conn.query(
     "INSERT INTO average (avg1, avg2, avg3, avg4, avg5) VALUES(?, ?, ?, ?, ?);",
@@ -80,7 +170,6 @@ app.post("/", function (req, res) {
     }
   );
 });
-
 
 
 //  未算平均值之原程式碼
@@ -163,12 +252,6 @@ app.post("/", function (req, res) {
 //   );
 // });
 
-app.get("/page2", (req, res) => {
-  res.render("page2.ejs");
-});
-app.get("/result", (req, res) => {
-  res.render("result_page.ejs");
-});
 
 // 使用靜態資源
 app.use(express.static(__dirname + "/public"));
